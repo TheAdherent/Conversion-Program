37d2edaa001525e7f94d9fc60524b84d9e2f81ed
Caleb Duff <me@calebduff.com>
2012-04-11 14:34:33 -0500

    Now the program asks if you would like to convert the temperature Celsius.

--- Conversion.java
+++ Conversion.java
@@ -1,10 +1,17 @@
 package conversion;
-
+import java.util.Scanner;
 public class Conversion {
 
+    enum whichtoConvert {
+    CelsiustoFahrenheit
+    }
+    
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
         Celsius ctof = new Celsius();
-
-        ctof.Process();
+        boolean choice = true;
+        System.out.println("Would you like to convert Celsius to Fahrenheit? If so, type true.");
+        choice = scnr.nextBoolean();
+        if (choice == true) ctof.Process();
     }
 }
