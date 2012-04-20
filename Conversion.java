116285a682b1ce6f8339cddea90297ef570c9268
Caleb Duff <me@calebduff.com>
2012-04-19 20:05:09 -0500

    Update Conversion.java

--- Conversion.java
+++ Conversion.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Conversion {
     
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-        Celsius ctof = new Celsius();
-        String choice;
+    Scanner scnr = new Scanner(System.in);
+	Celsius ctof = new Celsius();
+	String choice;
         
-        System.out.println("Would you like to convert Celsius to Fahrenheit? yes/no");
-        choice = scnr.next();
+	System.out.println("Would you like to convert Celsius to Fahrenheit? yes/no");
+	choice = scnr.next();
         
-        if (choice.equals("yes")) ctof.Process();
-        if (choice.equals("no")) System.out.println("Well, fine then!");
-     }
+	if (choice.equals("yes")) ctof.Process();
+	if (choice.equals("no")) System.out.println("Well, fine then!");
     }
+}
