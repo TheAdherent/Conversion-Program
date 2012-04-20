375e97501ee9e71e11728d4a13197d67d2d41d82
Caleb Duff <me@calebduff.com>
2012-04-19 20:06:25 -0500

    Update Celsius.java

--- Celsius.java
+++ Celsius.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 
 public class Celsius {
     void Process() {
-        Scanner scnr = new Scanner(System.in);
-        
-        double celsius, fahrenheit;
-        
-        System.out.println("Enter the temperature in Celsius: ");
-        celsius = scnr.nextDouble();
+    Scanner scnr = new Scanner(System.in);
 
-        fahrenheit = celsius * 9/5 + 32;
+	double celsius, fahrenheit;
 
-        System.out.println("\nTemperature in Celsius:");
-        System.out.println(celsius);
-        System.out.println("Temperature in Fahrenheit:");
-        System.out.println(fahrenheit);
-   
+	System.out.println("Enter the temperature in Celsius: ");
+	celsius = scnr.nextDouble();
+
+	fahrenheit = (((celsius * 9)/5) + 32);
+
+	System.out.println("\nTemperature in Celsius:");
+	System.out.println(celsius);
+	System.out.println("Temperature in Fahrenheit:");
+	System.out.println(fahrenheit);
     }
 }
