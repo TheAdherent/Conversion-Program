11c527c935fb5075659098113f9873046edf7c2f
Caleb Duff <me@calebduff.com>
2012-06-13 17:22:29 -0500

    Added fancier regex stuff
    Changed line 17
    String choice = new choice;
    to
    String choice = "^[\\s]*[fc]{1}$";

--- Celsius.java
+++ Celsius.java
@@ -1,38 +1,38 @@
 package conversion;
 import java.util.Scanner;
 
+
 public class Celsius {
     void ConvertCelsius() {
-        Scanner scnr = new Scanner(System.in);
-        
-        double celsius, fahrenheit;
-        
-        System.out.println("Enter the temperature in Celsius: ");
-        celsius = scnr.nextDouble();
-        
-        //Here is the formula:
-        fahrenheit = (celsius * 9/5) + 32;
+	Scanner scnr = new Scanner(System.in);
 
-        System.out.println("\nTemperature in Celsius:");
-        System.out.println(celsius);
-        System.out.println("Temperature in Fahrenheit:");
-        System.out.println(fahrenheit);
-      
+	double celsius, fahrenheit;
+
+	System.out.println("Enter the temperature in Celsius: ");
+	celsius = scnr.nextDouble();
+
+	//Here is the formula:
+	fahrenheit = (celsius * 9/5) + 32;
+
+	System.out.println("\nTemperature in Celsius:");
+	System.out.println(celsius);
+	System.out.println("Temperature in Fahrenheit:");
+	System.out.println(fahrenheit);
     }
     void ConvertFahrenheit() {
-        Scanner scnr = new Scanner(System.in);
-        
-        double celsius, fahrenheit;
-        
-        System.out.println("Enter the temperature in Fahrenheit: ");
-        fahrenheit = scnr.nextDouble();
-        
-        //Here is the formula:
-        celsius = (fahrenheit - 32) * 5/9;
+	Scanner scnr = new Scanner(System.in);
 
-        System.out.println("Temperature in Fahrenheit:");
-        System.out.println(fahrenheit);
-        System.out.println("\nTemperature in Celsius:");
-        System.out.println(celsius);
+	double celsius, fahrenheit;
+
+	System.out.println("Enter the temperature in Fahrenheit: ");
+	fahrenheit = scnr.nextDouble();
+
+	//Here is the formula:
+	celsius = (fahrenheit - 32) * 5/9;
+
+	System.out.println("\nTemperature in Fahrenheit:");
+	System.out.println(fahrenheit);
+	System.out.println("Temperature in Celsius:");
+	System.out.println(celsius);
     }
 }
--- Conversion.java
+++ Conversion.java
@@ -6,22 +6,24 @@
  */
 
 package conversion;
+import java.util.regex.Matcher;
 import java.util.Scanner;
+
 public class Conversion {
-    
+
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-        Celsius ctof = new Celsius();
-        String choice;
-        
-        System.out.println("What would you like to convert?");
-        System.out.println("Type f for Fahrenheit and c for Celsius.");
-        choice = scnr.next();
-        if (choice.equals("c") || choice.equals("C")) {ctof.ConvertCelsius();}
+	Scanner scnr = new Scanner(System.in);
+	Celsius ctof = new Celsius();
+	String choice = "^[\\s]*[fc]{1}$";
 
-        if (choice.equals("f") || choice.equals("F")) {ctof.ConvertFahrenheit();}
+	System.out.println("What would you like to convert?");
+	System.out.println("Type f for Fahrenheit and c for Celsius.");
+	choice = scnr.next();
 
-        else {System.out.println("Sorry, input not recognized.");}
+	if (choice.equals("c") || choice.equals("C")) {ctof.ConvertCelsius();}
 
+	if (choice.equals("f") || choice.equals("F")) {ctof.ConvertFahrenheit();}
+
+	else {System.out.println("Sorry, input not recognized.");}
     }
 }
